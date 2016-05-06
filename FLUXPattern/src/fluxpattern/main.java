e3914619f30322764f915949778bdadc97bf9c95
Richard Dahlgren <richard@richarddahlgren.se>
2016-05-07 01:05:55 +0200

    Update

--- FLUXPattern/src/fluxpattern/main.java
+++ FLUXPattern/src/fluxpattern/main.java
@@ -1,11 +1,27 @@
 package fluxpattern;
 
+import fluxpattern.store.TypeOneStore;
+import fluxpattern.view.TypeOneView;
+
 /**
  *
  * @author Richard
  */
-public class main {
+public class Main {
 
     public static void main(String[] args) {
+        new Main();
+    }
+    
+    public Main() {
+        TypeOneStore tos = new TypeOneStore();
+        Dispatcher.getInstance().registerStore(tos);
+        
+        TypeOneView tov = new TypeOneView();
+        tos.registerView(tov);
+        
+        tov.render();
+        
+        tov.triggerAction();
     }
 }
